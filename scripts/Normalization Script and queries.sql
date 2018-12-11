bd393f1f43a359688cd3cfaf28b550c19dd8f420
CSwendy <35074690+csWendy@users.noreply.github.com>
2018-12-11 10:54:44 -0500

    Delete Normalization Script and queries.sql
    delete unnecessary file

--- scripts/Normalization Script and queries.sql
+++
@@ -1,6 +0,0 @@
-
--- Normalization Script for School_performance table
-SELECT School_performance.*, Schools.graduation_rate, Schools.college_career_rate FROM School_performance JOIN Schools USING (dbn);
-
--- querys  
-select dbn,school_name,graduation_rate, college_career_rate from Schools group by dbn;
